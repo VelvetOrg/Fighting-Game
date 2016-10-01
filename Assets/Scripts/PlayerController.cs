186f7a2bf89ad6b6b19bc6a4a992505f52bbfc30
Cameron_Bell <TheDesignerPenguin@gmail.com>
2016-10-02 00:54:18 +1000

    Added basic character controller movement
    - Very snappy (Not at all smooth)
    - Not realistic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,73 @@
 using System.Collections;
 
 //Works on a physics based system
-[RequireComponent (typeof (Rigidbody))]
-[RequireComponent (typeof (CapsuleCollider))]
-public class PlayerController : MonoBehaviour {
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    //Since character controller do not have physics:
+    public float gravity = 20.0f;
+
+    //Collision proerties
+    public float groundOffset = 0.3f; //Stay off the ground slightly
+    public float antiJitter = 3.0f; //Fix collider rotating randomly
 
 	//Movement properties
-	public float moveSpeed = 10.0f;
+	public float moveSpeed = 8.0f; //Units to move in one second
+    public float airControl = 0.1f; //Limit movement in the air
+    public float jump = 10.0f; //Jump force - in units
 
-	//Needs to affect the rigidbody
-	Rigidbody body;
+	//Privates
+	CharacterController body; //Holds information on events
+    Vector3 contact; //When checking for collision
+    Vector3 direction; //Move dir
+    RaycastHit hit; //Generaic value
 
-	void Awake () { body = GetComponent<Rigidbody> (); }
+    float rayDistance;
+    bool grounded;
 
-	//Called by the player input class
-	public void Move(Vector2 axis) 
+    //Set variables
+    void Awake ()
+    {
+        //Get controller
+        body = GetComponent<CharacterController> ();
+
+        //Ray distance should be how far down is the bottom of the collider
+        //Assumes transform is in the ctner
+        rayDistance = (body.height / 2) + body.radius + groundOffset;
+    }
+
+    //Has a collision hit occured
+    void OnControllerColliderHit(ControllerColliderHit h) { contact = h.point; }
+
+    //Called by the player input class
+    public void Move(Vector2 axis) 
 	{
-		//Move smoothly
-		Vector2 moveAmount = axis * moveSpeed;
+		//Move when grounded
+        if(grounded)
+        {
+            //Direction holds the force to apply to the controller
+            //Convert to local space - so it will move relative to the mouse look script (once done)
+            direction = transform.TransformDirection(new Vector3(axis.x, -antiJitter, axis.y)) * moveSpeed;
 
-		//Apply to the body
-		body.velocity = new Vector3(moveAmount.x, body.velocity.y, moveAmount.y);
+            //Jump - apply firce to the direction vector
+            if (Input.GetButton("Jump")) { direction.y = jump; }
+        }
+        else
+        {
+            //Still let player move but limit it
+            //Because they are in the air
+            //Alos convert to local space
+            direction = transform.TransformDirection(new Vector3(axis.x * moveSpeed * airControl, direction.y, axis.y * moveSpeed * airControl));
+        }
+
+        //Apply gavity
+        direction.y -= gravity * Time.deltaTime;
+
+        //Move
+        //Body returns the collision types
+        //Convert that to a grounded stat
+        //https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+        CollisionFlags g = body.Move(direction * Time.deltaTime);
+        grounded = (g & CollisionFlags.Below) != 0;
 	}
 }
