d1078847ffdbab990ea8f829224668e7d18952ba
Cameron_Bell <TheDesignerPenguin@gmail.com>
2016-10-01 23:01:26 +1000

    Updated movement to a velocity based system
    It may seem worse for now. But using add force creates alot of problems
    in the future.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,49 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Works on a physics based system
 [RequireComponent (typeof (Rigidbody))]
 [RequireComponent (typeof (CapsuleCollider))]
-
 public class PlayerController : MonoBehaviour {
 
-	public float movementFactor = 10.0f;
-	public float maximumVelocity = 10.0f;
-	public bool canJump = true;
-	public float jumpHeight = 2.0f;
-	private bool isGrounded = true;
-	Rigidbody playerRigidbody;
+	//Movement properties
+	public float moveSpeed = 10.0f;
 
-	void Awake () {
-		playerRigidbody = GetComponent<Rigidbody> ();
-	}
+	//Needs to affect the rigidbody
+	Rigidbody body;
 
-	void FixedUpdate () {
-		if (isGrounded) {
-			Vector3 finalVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			finalVelocity = transform.TransformDirection(finalVelocity);
-			finalVelocity *= movementFactor;
+	void Awake () { body = GetComponent<Rigidbody> (); }
 
-			Vector3 currentVelocity = GetComponent<Rigidbody>().velocity;
-			Vector3 velocityChange = (finalVelocity - currentVelocity);
-			velocityChange.x = Mathf.Clamp(velocityChange.x, -maximumVelocity, maximumVelocity);
-			velocityChange.z = Mathf.Clamp(velocityChange.z, -maximumVelocity, maximumVelocity);
-			velocityChange.y = 0;
-			playerRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-			//playerRigidbody.MovePosition(velocityChange + transform.position * movementFactor);
-			// Jump
-			if (canJump && Input.GetKeyDown(KeyCode.Space)) {
-				playerRigidbody.velocity = new Vector3(currentVelocity.x, Mathf.Sqrt(2 * jumpHeight * 10), currentVelocity.z);
-			}
-		}
+	//Called by the player input class
+	public void Move(Vector2 axis) 
+	{
+		//Move smoothly
+		Vector2 moveAmount = axis * moveSpeed;
 
-		// We apply gravity manually for more tuning control
-		playerRigidbody.AddForce(new Vector3 (0, -10 * GetComponent<Rigidbody>().mass, 0));
-		//playerRigidbody.MovePosition(transform.position + new Vector3 (0, -10 * playerRigidbody.mass, 0) * movementFactor);
-
-		isGrounded = false;
-	}
-
-	void OnCollisionStay () {
-		isGrounded = true;    
+		//Apply to the body
+		body.velocity = new Vector3(moveAmount.x, body.velocity.y, moveAmount.y);
 	}
 }
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class allows any controller based functions to get user input
+//Because it is a wrapper, the code is versitile
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour 
+{
+	//Input axis will be normalized for multi keys
+	Vector2 inputAxis; //{ public get; private set; }
+	PlayerController attached;
+
+	//Get
+	void Awake()
+	{
+		//Initialze values
+		inputAxis = Vector2.zero;
+		attached = GetComponent<PlayerController>();
+	}
+	
+	//Later events may be used
+	void Update() 
+	{
+		//Get the basic unity input axis
+		int horizontal = (int)Input.GetAxisRaw("Horizontal");
+		int vertical = (int)Input.GetAxisRaw("Vertical");
+
+		//What should the multi key affect be
+		float mul = 1.0f;
+
+		//Are two keys down
+		if(Mathf.Abs(horizontal) + Mathf.Abs(vertical) == 2) { mul = 0.707106f; } // 1 / (sqrt(2))
+
+		//Pack into a vec 2
+		inputAxis = new Vector2(horizontal, vertical) * mul;
+
+		//Force player to move via the axis
+		attached.Move(inputAxis);
+	}
+}
