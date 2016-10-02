d80138cfab65bc4005d7603702c34819b9436314
Cameron_Bell <TheDesignerPenguin@gmail.com>
2016-10-02 23:07:31 +1100

    Removed git ignore (for now)

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Owned by Velvet-Org. Copyright 2016 - 
+ * This code is licenced under: Apache 2.0
+ * Cameron Bell, Ruchir Bapat 
+*/
+
+using UnityEngine;
+using System.Collections;
+
+//Needs to inherit from meele weapon
+public class Gun : RangedWeapon
+{
+    //There are three gun modes
+    [System.Serializable]
+    public enum Mode { Automatic, Burst, Manual };
+    public Mode fireMode = Mode.Manual;
+    public float fireRate = 100.0f;
+
+    //Check for manual fire
+    bool triggerReleased;
+
+    //Actually fire the gun
+    protected override void Fire()
+    {
+        print("Shot fired.");
+    }
+
+    //When buttons are pressed
+    public override void AttackHeld() { Fire(); triggerReleased = false; }
+    public override void AttackReleased() { triggerReleased = true; }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * Owned by Velvet-Org. Copyright 2016 - 
+ * This code is licenced under: Apache 2.0
+ * Cameron Bell, Ruchir Bapat 
+*/
+
+//Namespaces
+using UnityEngine;
+
+//A pure abstract class used by the player
+public abstract class Weapon
+{
+
+}
+
+//Every wepon in the game inherits from this class
+//They share some properties in common
+// - Gun
+// - Bow and arrow
+public class RangedWeapon : MonoBehaviour
+{
+    //Expand on this later
+    [Header("Ranged Weapon")]
+    public Transform end; //The barrel or fire point
+    public float damage = 1.0f; //Result of the attack
+    public float attackDuration = 0.5f; //How long each shot lasts for
+    public float range = 50.0f; //Distance used by the ray
+
+    //Virtual functions
+    protected virtual void Fire() { }
+
+    //Called when the user is holding down fire
+    public virtual void AttackReleased() { }
+    public virtual void AttackHeld() { Fire(); }
+}
+
+//Any meele weapon inherits from this
+// - Swords
+// - Knives
+//Will do this later...
+public class MeeleWeapon : MonoBehaviour { }
